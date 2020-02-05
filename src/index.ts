660718cbdf04e90c5ca54382db676bc108d2da39
whalemare <nanostet@gmail.com>
2020-02-05 18:22:50 +0700

    Add loading sample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import ora from 'ora';
+
 console.log("Hello world 2")
+
+const loader = ora().start("Starting loading")
+setTimeout(() => {
+  loader.text = 'Loading in progress';
+  setTimeout(() => {
+    loader.stopAndPersist({
+      text: "Loading is finished"
+    })
+  }, 2000)
+}, 2000);
